e935ebf18040031201b22e0239e516039f382630
keiko.kirameki <bestpricezakka@gmail.com>
2019-12-19 07:24:45 +0000

    add column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,11 @@
 class TasksController < ApplicationController
-before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
     @tasks = Task.all
   end
 
   def show
-    set_task
   end
 
   def new
@@ -14,24 +13,21 @@ before_action :set_task, only: [:show, :edit, :update, :destroy]
   end
 
   def create
-     @task = Task.new(task_params)
+    @task = Task.new(task_params)
 
     if @task.save
       flash[:success] = 'Task が正常に作成されました'
       redirect_to @task
     else
-      flash.now[:danger] = 'Task が作成されませんでした'
+      flash.now[:danger] = 'Task が投稿されませんでした'
       render :new
     end
   end
 
   def edit
-    set_task
   end
 
   def update
-     set_task
-
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
       redirect_to @task
@@ -42,21 +38,20 @@ before_action :set_task, only: [:show, :edit, :update, :destroy]
   end
 
   def destroy
-    set_task
     @task.destroy
 
     flash[:success] = 'Task は正常に削除されました'
     redirect_to tasks_url
   end
-  
+
   private
 
   def set_task
-    @task = task.find(params[:id])
+    @task = Task.find(params[:id])
   end
-  
+
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
   
 end
