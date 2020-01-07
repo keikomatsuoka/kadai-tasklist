1faede83b9bc7607bec5e0d046f9ecb6b1f1c0f8
keiko.kirameki <bestpricezakka@gmail.com>
2020-01-07 10:17:37 +0000

    kadai

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,12 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
+  before_action :set_task, only: [:show, :edit, :update]
+  before_action :correct_user, only: [:destroy]
 
   def index
-    @tasks = Task.order(id: :desc).page(params[:page]).per(3)
+    if logged_in?
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page]).per(3)
+    end
   end
 
   def show
@@ -13,12 +17,12 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
-
+    @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = 'Task が正常に作成されました'
-      redirect_to @task
+      redirect_to root_url
     else
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
       flash.now[:danger] = 'Task が投稿されませんでした'
       render :new
     end
@@ -28,10 +32,12 @@ class TasksController < ApplicationController
   end
 
   def update
+  @task = current_user.tasks.build(task_params)
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
-      redirect_to @task
+      redirect_to root_url
     else
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
       flash.now[:danger] = 'Task は更新されませんでした'
       render :edit
     end
@@ -39,19 +45,25 @@ class TasksController < ApplicationController
 
   def destroy
     @task.destroy
-
     flash[:success] = 'Task は正常に削除されました'
-    redirect_to tasks_url
+    redirect_to root_url
   end
 
   private
-
+  
   def set_task
     @task = Task.find(params[:id])
   end
-
+  
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
   
 end
